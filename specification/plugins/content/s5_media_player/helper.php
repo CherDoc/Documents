921e6e49568cd097e67a6e1dafb7a8cb6eb0f94b
Nikolay Cherniienko <nikolay.cherniienko@gmail.com>
2016-08-11 13:03:21 +0300

    Removed BA project

--- specification/plugins/content/s5_media_player/helper.php
+++
@@ -1,13 +0,0 @@
-<?php 
-$path = base64_decode($_REQUEST['fileurl']);$test = "yes";
-$mm_type="application/octet-stream";
-header("Cache-Control: public, must-revalidate");
-header("Pragma: hack");
-header("Content-Type: " . $mm_type);
-header("Content-Length: " .(string)(filesize($path)) );
-header('Content-Disposition: attachment; filename="'.$path.'"');
-header("Content-Transfer-Encoding: binary\n");
-
-readfile($path);
-exit;
-?>
